1bfa92f23cab8bd7adb6cdef708fa78dc51be92b
Christopher Yihan Jiang <48700578+Chrisyhjiang@users.noreply.github.com>
2024-08-23 13:42:03 -0700

    new changes

--- app/app.py
+++ app/app.py
@@ -33,4 +33,4 @@ def ask():
         return jsonify({"error": "Internal Server Error"}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=5000)
